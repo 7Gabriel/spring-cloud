341f84f1f452ca9180ec6477611845ea7502b29f
joao1gabriel <joao1.gabrielsilva@gmail.com>
2020-12-30 14:42:51 -0300

    feat: create worker

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT into tb_worker(name, daily_income) values ('Bob', 200.0);
+INSERT into tb_worker(name, daily_income) values ('Joao', 300.0);
+INSERT into tb_worker(name, daily_income) values ('Maria', 400.0);
