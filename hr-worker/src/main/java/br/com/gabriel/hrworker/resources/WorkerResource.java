9f6eca0ba62a5d48457e5fd31c569ac28237cb9d
joao1gabriel <joao1.gabrielsilva@gmail.com>
2020-12-30 17:25:10 -0300

    feat: ribbon load balancer

--- hr-worker/src/main/java/br/com/gabriel/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/gabriel/hrworker/resources/WorkerResource.java
@@ -2,6 +2,7 @@ package br.com.gabriel.hrworker.resources;
 
 import br.com.gabriel.hrworker.entities.Worker;
 import br.com.gabriel.hrworker.repositories.WorkerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
