99cf29de2100682af0cf81b03ec5084e75a5f448
joao1gabriel <joao1.gabrielsilva@gmail.com>
2020-12-30 19:53:50 -0300

    feat: add circuit break

--- hr-payroll/src/main/java/br/com/gabriel/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/gabriel/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package br.com.gabriel.hrpayroll.resources;
 
 import br.com.gabriel.hrpayroll.entities.Payment;
 import br.com.gabriel.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,20 @@ public class PaymentResource {
 
     @Autowired
     private PaymentService paymentService;
-    
+
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable("workerId") Long workerId, @PathVariable("days") Integer days){
         Payment payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.status(HttpStatus.OK).body(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = Payment.builder()
+                .name("Brann")
+                .dailyIncome(300.00)
+                .days(days)
+                .build();
         return ResponseEntity.status(HttpStatus.OK).body(payment);
     }
 
