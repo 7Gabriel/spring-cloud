df6e79d633c3396df2c6b6a14b81941a60de18c9
joao1gabriel <joao1.gabrielsilva@gmail.com>
2020-12-30 17:10:05 -0300

    feat: add feign

---
+++ hr-payroll/src/main/java/br/com/gabriel/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package br.com.gabriel.hrpayroll.feignclients;
+
+import br.com.gabriel.hrpayroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(value = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Worker> findById(@PathVariable("id") Long id);
+
+}
