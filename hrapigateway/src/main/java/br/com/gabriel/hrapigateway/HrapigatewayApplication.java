c931aa066fb02777dd737a196767f3dc3e7512f8
joao1gabriel <joao1.gabrielsilva@gmail.com>
2021-01-04 10:21:56 -0300

    feat: create zuul project

---
+++ hrapigateway/src/main/java/br/com/gabriel/hrapigateway/HrapigatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.hrapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrapigatewayApplication.class, args);
+	}
+
+}
---
+++ hrapigateway/src/test/java/br/com/gabriel/hrapigateway/HrapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
